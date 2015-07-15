caa1dc741c675b8dc3e34fb868e1b6ddc095f8da
Dean Laurea <deanlaurea@icloud.com>
2015-07-15 06:48:12 -0500

    Master-Detail User Interface

---
+++ Algorhythm/Algorhythm/PlaylistDetailViewController.h
@@ -0,0 +1,15 @@
+//
+//  PlaylistDetailViewController.h
+//  Algorhythm
+//
+//  Created by Dean Laurea on 7/15/15.
+//  Copyright (c) 2015 Dean Laurea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlaylistDetailViewController : UIViewController
+
+
+
+@end
---
+++ Algorhythm/Algorhythm/PlaylistDetailViewController.m
@@ -0,0 +1,28 @@
+//
+//  PlaylistDetailViewController.m
+//  Algorhythm
+//
+//  Created by Dean Laurea on 7/15/15.
+//  Copyright (c) 2015 Dean Laurea. All rights reserved.
+//
+
+#import "PlaylistDetailViewController.h"
+
+@interface PlaylistDetailViewController ()
+
+@end
+
+@implementation PlaylistDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
