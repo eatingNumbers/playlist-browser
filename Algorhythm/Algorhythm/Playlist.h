5121540412d694b26897520367816aec2874863e
Dean Laurea <deanlaurea@icloud.com>
2015-07-15 15:19:03 -0500

    Adding a Playlist

---
+++ Algorhythm/Algorhythm/Playlist.h
@@ -0,0 +1,21 @@
+//
+//  Playlist.h
+//  Algorhythm
+//
+//  Created by Dean Laurea on 7/15/15.
+//  Copyright (c) 2015 Dean Laurea. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIkit.h>
+
+@interface Playlist : NSObject
+
+@property (strong, nonatomic) NSString *playlistTitle;
+@property (strong, nonatomic) NSString *playlistDescription;
+@property (strong, nonatomic) UIImage *playlistIcon;
+@property (strong, nonatomic) UIImage *playlistIconLarge;
+@property (strong, nonatomic) NSArray *playlistArtists;
+@property (strong, nonatomic) UIImage *backgroundColor;
+
+@end
---
+++ Algorhythm/Algorhythm/Playlist.m
@@ -0,0 +1,13 @@
+//
+//  Playlist.m
+//  Algorhythm
+//
+//  Created by Dean Laurea on 7/15/15.
+//  Copyright (c) 2015 Dean Laurea. All rights reserved.
+//
+
+#import "Playlist.h"
+
+@implementation Playlist
+
+@end
