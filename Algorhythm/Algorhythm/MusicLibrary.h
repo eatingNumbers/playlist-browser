e09eeb4b37fd0b07cd2add0eb132657fa660ee6e
Dean Laurea <deanlaurea@icloud.com>
2015-07-15 14:30:53 -0500

    Creating a Music Library

---
+++ Algorhythm/Algorhythm/MusicLibrary.h
@@ -0,0 +1,15 @@
+//
+//  MusicLibrary.h
+//  Algorhythm
+//
+//  Created by Dean Laurea on 7/15/15.
+//  Copyright (c) 2015 Dean Laurea. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MusicLibrary : NSObject
+
+@property (strong, nonatomic) NSArray *library;
+
+@end
