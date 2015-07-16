6ad726f60f2bb7e0b060e4e9e15c6ecf034f84df
Dean Laurea <deanlaurea@icloud.com>
2015-07-16 07:57:06 -0500

    Passing the Playlist Object

--- Algorhythm/Algorhythm/PlaylistDetailViewController.h
+++ Algorhythm/Algorhythm/PlaylistDetailViewController.h
@@ -8,9 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
+@class Playlist;
+
 @interface PlaylistDetailViewController : UIViewController
 
 @property (weak, nonatomic) IBOutlet UILabel *buttonPressLabel;
-@property (strong, nonatomic) NSString *segueLabelText;
+@property (strong, nonatomic) Playlist *playlist;
 
 @end
--- Algorhythm/Algorhythm/PlaylistMasterViewController.m
+++ Algorhythm/Algorhythm/PlaylistMasterViewController.m
@@ -8,6 +8,7 @@
 
 #import "PlaylistMasterViewController.h"
 #import "PlaylistDetailViewController.h"
+#import "Playlist.h"
 
 @interface PlaylistMasterViewController ()
 
@@ -19,6 +20,8 @@
     [super viewDidLoad];
 
     [self.aButton setTitle:@"Press me" forState:UIControlStateNormal];
+    Playlist *playlist = [[Playlist alloc] initWithIndex:0];
+    self.playlistImageView0.image = playlist.playlistIcon;
     
 }
 
@@ -30,8 +33,8 @@
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     if ([segue.identifier isEqual:@"showPlaylistDetail"]) {
         PlaylistDetailViewController *playlistDetailController = (PlaylistDetailViewController *)segue.destinationViewController;
-        playlistDetailController.segueLabelText = @"Yay! You pressed the button";
-        
+
+        playlistDetailController.playlist = [[Playlist alloc] initWithIndex:1];
         
     }
 }
